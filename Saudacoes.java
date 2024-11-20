2bc68d365da3d63967afa6ab2a3dc2b8b6bf6e74
Paulo <paulohsantosrocha@gmail.com>
2024-11-20 15:08:24 -0300

    exercícios já resolvidos

---
+++ Saudacoes.java
@@ -0,0 +1,12 @@
+public class Saudacoes {
+    public static void main(String[] args){
+        String name = "John Doe";
+        String address = "Rua das Flores, 123";
+        double monthSalary = (double) 6750.35;
+        String date = "01/01/2021";
+
+        System.out.println("Eu "+name+", morando no endereço "+address+", confirmo que recebi o salário de R$"+monthSalary+" na data "+date+".");    
+    }
+} 
+
+
